e84478972c5649a5fbd4b8054a4171ad9a5489f0
smhrd <smhrd@DESKTOP-F4BC2RB>
2020-06-23 17:46:15 +0900

    소스코드 수정

--- ex0623_gitTest/src/ex0623_gitTest/Hellojava.java
+++ ex0623_gitTest/src/ex0623_gitTest/Hellojava.java
@@ -3,7 +3,8 @@ package ex0623_gitTest;
 public class Hellojava {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hellojava");
+		System.out.println("Hello");    
 
 	}
 
